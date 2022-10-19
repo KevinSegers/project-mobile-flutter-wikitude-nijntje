b5b619f3570f6999f30664ed610b0743cc286242
KevinSegers <r0793406@student.thomasmore.be>
2022-10-19 15:11:31 +0200

    app working with click on book showing dino's

--- lib/widgets/books_list.dart
+++ lib/widgets/books_list.dart
@@ -2,16 +2,26 @@
 // click action naar wikitude bij beschikbare boeken
 // eventueel like button
 
-import 'package:flutter/material.dart';
-
-import 'book_item.dart';
-
-class BooksList extends StatelessWidget {
-  const BooksList({Key? key}) : super(key: key);
-
 //TODO refactor from hard coded books to dynamic books => see post_list.dart in de NYT app
 //TODO klikbare items met functie die controleert op beschikbaarheid => weergeven in tekst
 //bij het item en bij klikken popup ofzo dat het item nog niet beschikbaar is
+import 'package:augmented_reality_plugin_wikitude/wikitude_plugin.dart';
+import 'package:augmented_reality_plugin_wikitude/wikitude_response.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/ardino.dart';
+import 'book_item.dart';
+
+class BooksList extends StatefulWidget {
+  const BooksList({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BookListState();
+}
+
+class _BookListState extends State<BooksList> {
+  List<String> features = ["image_tracking"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +37,10 @@ class BooksList extends StatelessWidget {
             title: "Boeken Nijntje",
             imageUrl: "https://i.postimg.cc/RFrDZ6zc/Nijntjeindespeeltuin.jpg",
             abstract: "Nijntje in de speeltuin",
-            onTapped: () {},
+            onTapped: () {
+              navigateToDinos();
+              const Text("Scan the Dino's!");
+            },
           ),
           BookItem(
             title: "Boeken Nijntje",
@@ -44,5 +57,40 @@ class BooksList extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void navigateToDinos() {
+    debugPrint("Wij gaan naar dino's");
+
+    checkDeviceCompatibility().then((value) => {
+          if (value.success)
+            {
+              requestARPermissions().then((value) => {
+                    if (value.success)
+                      {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ArDinoPage()),
+                        )
+                      }
+                    else
+                      {
+                        debugPrint("AR permissions denied"),
+                        debugPrint(value.message)
+                      }
+                  })
+            }
+          else
+            {debugPrint("Device incompatible"), debugPrint(value.message)}
+        });
+  }
+
+  Future<WikitudeResponse> checkDeviceCompatibility() async {
+    return await WikitudePlugin.isDeviceSupporting(features);
+  }
+
+  Future<WikitudeResponse> requestARPermissions() async {
+    return await WikitudePlugin.requestARPermissions(features);
   }
 }
