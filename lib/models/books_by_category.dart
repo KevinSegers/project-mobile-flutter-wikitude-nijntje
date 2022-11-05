c2297f3e655bf2caab9d36f2aac3f26b5d08911d
KevinSegers <r0793406@student.thomasmore.be>
2022-11-05 10:06:53 +0100

    removed pages from booksByCategory

--- lib/models/books_by_category.dart
+++ lib/models/books_by_category.dart
@@ -12,77 +12,38 @@ String booksByCategoryToJson(List<BooksByCategory> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class BooksByCategory {
-  //TODO Refactor to only get books
   BooksByCategory({
-    required this.bookTitle,
+    required this.title,
     required this.author,
     required this.favorite,
     required this.available,
     required this.coverImageUrl,
     required this.backCoverImageUrl,
-    required this.pagesFromBook,
   });
 
-  String bookTitle;
+  String title;
   String author;
   bool favorite;
   bool available;
   String coverImageUrl;
   String backCoverImageUrl;
-  List<PagesFromBook> pagesFromBook;
 
   factory BooksByCategory.fromJson(Map<String, dynamic> json) =>
       BooksByCategory(
-        bookTitle: json["bookTitle"],
+        title: json["title"],
         author: json["author"],
         favorite: json["favorite"],
         available: json["available"],
         coverImageUrl: json["coverImageUrl"],
         backCoverImageUrl: json["backCoverImageUrl"],
-        pagesFromBook: List<PagesFromBook>.from(
-            json["pagesFromBook"].map((x) => PagesFromBook.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
-        "bookTitle": bookTitle,
+        "title": title,
         "author": author,
         "favorite": favorite,
         "available": available,
         "coverImageUrl": coverImageUrl,
         "backCoverImageUrl": backCoverImageUrl,
-        "pagesFromBook":
-            List<dynamic>.from(pagesFromBook.map((x) => x.toJson())),
-      };
-}
-
-class PagesFromBook {
-  PagesFromBook({
-    required this.id,
-    required this.pageNumber,
-    this.items,
-    required this.seen,
-    required this.bookTitle,
-  });
-
-  String id;
-  int pageNumber;
-  dynamic items;
-  bool seen;
-  String bookTitle;
-
-  factory PagesFromBook.fromJson(Map<String, dynamic> json) => PagesFromBook(
-        id: json["id"],
-        pageNumber: json["pageNumber"],
-        items: json["items"],
-        seen: json["seen"],
-        bookTitle: json["bookTitle"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "pageNumber": pageNumber,
-        "items": items,
-        "seen": seen,
-        "bookTitle": bookTitle,
       };
 }
