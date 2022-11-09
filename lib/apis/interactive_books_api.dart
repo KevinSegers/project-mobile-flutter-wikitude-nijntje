84d52b31039be8517a67dc26d208d078d7a302e0
KevinSegers <r0793406@student.thomasmore.be>
2022-11-09 19:27:09 +0100

    + updateBook, change name to Book

--- lib/apis/interactive_books_api.dart
+++ lib/apis/interactive_books_api.dart
@@ -1,16 +1,16 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import '../models/books_by_category.dart';
+import '../models/book.dart';
 import '../models/category.dart';
 import '../models/page.dart';
 
 class InteractiveBooksApi {
   //Kevin
-  //static String edgeService = '192.168.0.199:8050';
+  static String edgeService = '192.168.0.199:8050';
 
   //michal
-  static String edgeService = 'slimy-trees-greet-84-197-131-83.loca.lt';
+  //static String edgeService = 'slimy-trees-greet-84-197-131-83.loca.lt';
 
   //FetchCategoriesWithUrls
   static Future<List<Category>> fetchCategoriesWithUrls() async {
@@ -29,8 +29,7 @@ class InteractiveBooksApi {
   }
 
   //FetchBooksByCategory
-  static Future<List<BooksByCategory>> fetchBooksByCategory(
-      String category) async {
+  static Future<List<Book>> fetchBooksByCategory(String category) async {
     category = category.toUpperCase();
     var url =
         Uri.http(edgeService, '/interactivebooks/booksbycategory/$category');
@@ -39,21 +38,38 @@ class InteractiveBooksApi {
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
-      return jsonResponse
-          .map((books) => BooksByCategory.fromJson(books))
-          .toList();
+      return jsonResponse.map((books) => Book.fromJson(books)).toList();
     } else {
       throw Exception('Failed to load books');
     }
   }
 
   //update Book
+  static Future<Book> updateBook(
+    Book book,
+  ) async {
+    var url = Uri.http(edgeService, 'interactivebooks/updatebook');
+    final response = await http.put(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(book),
+    );
 
-  //updateSeen
+    if (response.statusCode == 200) {
+      return Book.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+
+  //update Seen Page
   static Future<Page> updatePage(
     Page page,
   ) async {
-    var url = Uri.http(edgeService, 'interactivebooks/');
+    var url = Uri.http(edgeService,
+        'interactivebooks/'); //TODO ? moet hier geen pages achter??
     final response = await http.put(
       url,
       headers: <String, String>{
