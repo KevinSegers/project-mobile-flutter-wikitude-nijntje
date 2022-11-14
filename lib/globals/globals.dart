a904b21f797be0afd2088f1263402f6705cdcae4
KevinSegers <r0793406@student.thomasmore.be>
2022-11-14 16:36:30 +0100

    new global for okteto

--- lib/globals/globals.dart
+++ lib/globals/globals.dart
@@ -1,7 +1,11 @@
 library globals;
 
+//Okteto Hosting
+const String globalEdgeService =
+    'brank-edge-service-kevinsegers.cloud.okteto.net';
+
 //Kevin
-const String globalEdgeService = '192.168.0.199:8050';
+//const String globalEdgeService = '192.168.0.199:8050';
 
 //michal
 //const String globalEdgeService = '192.168.0.221:8050';
